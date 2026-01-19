530e2b07941d0b3bf7fa255e9521c674bb610ed9
scottf <scottfauerbach@gmail.com>
2026-01-19 12:02:59 -0500

    fix copyright

--- src/main/java/io/nats/json/JsonParser.java
+++ src/main/java/io/nats/json/JsonParser.java
@@ -1,6 +1,4 @@
-// Copyright 2023 The NATS Authors
-//
-// Modifications Copyright 2025-2026 Synadia Communications, Inc.
+// Copyright 2023-2026 The NATS Authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
